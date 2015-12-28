5efe4b967d37273df103ab4dc6f42a69d475592c
rhnvrm <rohanverma2004@gmail.com>
2015-12-29 00:07:41 +0530

    fixed installation bug where sync setting was not stored

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -5,7 +5,7 @@ chrome.extension.sendMessage({}, function(response) {
 
 		// ----------------------------------------------------------
 		// This part of the script triggers when page is done loading
-		console.log("Hello. This message was sent from scripts/inject.js");
+		//console.log("Hello. This message was sent from scripts/inject.js");
 		// ----------------------------------------------------------
 		var current = window.location.href;
 
@@ -44,7 +44,40 @@ chrome.extension.sendMessage({}, function(response) {
 
 		    }
 		    else{
-		    	console.log('Back2Work: Error! Retrival of information failed!')
+		    	console.log('Back2Work: Error! Retrival of information failed! Resetting the Blacklist');
+		    	
+		    	var blist = 'facebook.com,reddit.com';
+
+		    	chrome.storage.sync.set({
+				    blackList: blist
+				  });
+
+		    	sites = blist.split(',');
+
+				found = 0;
+
+				for(var i in sites){
+					//console.log(sites[i]);
+
+					if(current.indexOf(sites[i]) != '-1' && found == 0){
+
+						found = 1;
+
+						var bar = document.createElement("div");
+						bar.innerHTML = "GET BACK TO WORK!";
+						bar.style.color = "white"
+						bar.style.backgroundColor = "dodgerblue";
+						bar.style.textAlign = "center";
+						document.body.insertBefore(bar, document.body.firstChild);
+						bar.style.position="fixed"; 
+						bar.style.top ="1%";
+						bar.style.left ="50%";
+						bar.style.padding = "2px";
+						bar.style.zIndex = "10000";
+						bar.style.borderRadius = "5px";
+
+					}
+				}
 		    }
 		});
 
