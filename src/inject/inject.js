0054c19937e0b64c9e4d012591c3509bfa630baf
rhnvrm <rohanverma2004@gmail.com>
2015-12-28 23:22:19 +0530

    now only displays message on fb

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -7,20 +7,22 @@ chrome.extension.sendMessage({}, function(response) {
 		// This part of the script triggers when page is done loading
 		console.log("Hello. This message was sent from scripts/inject.js");
 		// ----------------------------------------------------------
+		var current = window.location.href;
 
-		var bar = document.createElement("div");
-		bar.innerHTML = "GET BACK TO WORK!";
-		bar.style.color = "white"
-		bar.style.backgroundColor = "dodgerblue";
-		bar.style.textAlign = "center";
-		document.body.insertBefore(bar, document.body.firstChild);
-		bar.style.position="fixed"; 
-		bar.style.top ="1%";
-		bar.style.left ="50%";
-		bar.style.padding = "2px";
-		bar.style.zIndex = "10000";
-		bar.style.borderRadius = "5px";
-
+		if(current.indexOf('facebook') != '-1'){
+			var bar = document.createElement("div");
+			bar.innerHTML = "GET BACK TO WORK!";
+			bar.style.color = "white"
+			bar.style.backgroundColor = "dodgerblue";
+			bar.style.textAlign = "center";
+			document.body.insertBefore(bar, document.body.firstChild);
+			bar.style.position="fixed"; 
+			bar.style.top ="1%";
+			bar.style.left ="50%";
+			bar.style.padding = "2px";
+			bar.style.zIndex = "10000";
+			bar.style.borderRadius = "5px";
+		}
 	}
 	}, 10);
 });
